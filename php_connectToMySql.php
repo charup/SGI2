a846d022e9ef5b3eff216a4b2690e3fc31542b2f
charup <charu_cambridge@yahoo.co.in>
2015-09-14 00:14:00 -0500

    commit second changes to php connect to MySql

--- php_connectToMySql.php
+++ php_connectToMySql.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username = "root";//"username";
+$password = "UnivPass123";//"password";
 	$dbname = "mydb";
 
 // Create connection
